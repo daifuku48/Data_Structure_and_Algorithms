24a4ee4fb6b4c56961d6a42123e7c5797efa8342
danil <natalya.haritonov.72@gmail.com>
2023-05-25 22:02:10 +0300

    adding and removing linked list

--- src/main/kotlin/LinkedList/LinkedList.kt
+++ src/main/kotlin/LinkedList/LinkedList.kt
@@ -2,13 +2,37 @@ package LinkedList
 
 class LinkedList<T>() {
     private var head : ListNode<T>? = null
+    private var last : ListNode<T>? = null
 
-    fun add(data: T)
+    fun isEmpty() = head == null
+
+    fun add(data: T) {
+        val p = ListNode(data)
+        if (isEmpty()) {
+            head = p
+            last = p
+            return
+        }
+        last?.next = p
+        last = p
+    }
+
+    fun removeFirst()
     {
-        head?.data = data
-        head = head?.next
+        if (isEmpty()) return
+        val value = head
+        head = value?.next
     }
 
 
-
+    fun print()
+    {
+        if (isEmpty()) return
+        var first = head
+        while(first != null)
+        {
+            print(first.data.toString() + " ")
+            first = first.next
+        }
+    }
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,10 @@
-fun main() {
+import LinkedList.LinkedList
 
+fun main() {
+    val list = LinkedList<Int>()
+    list.add(4)
+    list.add(3)
+    list.add(1)
+    list.removeFirst()
+    list.print()
 }
